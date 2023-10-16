f50d1dc39a2b5d9a83360353734a43069a7a31c6
FelixCCN <145447480+FelixCCN@users.noreply.github.com>
2023-10-17 01:06:56 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,8 @@
 var vg_1 = "map.json";
 var vg_2 = "chart.json";
+var vg_3 ="area_chart.json"
+var vg_4 ="pie_chart.json"
 vegaEmbed("#map_graph", vg_1).then(function(result) {}).catch(console.error);
 vegaEmbed("#chart_graph", vg_2).then(function(result) {}).catch(console.error);
+vegaEmbed("#area_graph", vg_3).then(function(result) {}).catch(console.error);
+vegaEmbed("#pie_chart", vg_4).then(function(result) {}).catch(console.error);
